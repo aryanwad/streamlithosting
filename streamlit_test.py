1c358608ff95e30460e0ace394976f97a1178129
AryanWadhwa05 <73252745+AryanWadhwa05@users.noreply.github.com>
2022-01-31 21:54:23 -0500

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -31,7 +31,7 @@ import statistics as stats
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 
-st.sidebar.image("AWlogo1.png")
+st.sidebar.image("AWlogo2.png")
 # Streamlit title
 st.title("Stock Market Analysis")
 
